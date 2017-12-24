9710b76624d4e517c2ca26fe223c8a0028923334
tungtran3012 <32742486+tungtran3012@users.noreply.github.com>
2017-12-25 05:25:42 +0700

    Test_InteractiveDataFriendRelation_Model
    Test file InteractiveDataFriendRelation in Model

---
+++ tests/unit/Test_InteractiveDataFriendRelation_Model.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+class Test_InteractiveDataFriendRelation_Model extends \PHPUnit\Framework\TestCase 
+{
+	public function test_GetFriendRelation_method1()
+	{
+		$test = new \InteractiveDataFriendRelation;
+		$this->assertInternalType('object', $test->GetFriendRelation("5a35e17dd7d69e7bfb31379e"));
+		$this->assertCount(1, $test->GetFriendRelation("5a35e518f3994baad05a85d2"));
+	}
+	
+}
+?>
