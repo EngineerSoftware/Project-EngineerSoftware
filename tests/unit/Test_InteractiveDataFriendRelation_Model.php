b49491bddd4435f1cf01bfd6d81628c7b1c95058
tungtran3012 <32742486+tungtran3012@users.noreply.github.com>
2017-12-25 05:29:24 +0700

    Update Test_InteractiveDataFriendRelation_Model.php

--- tests/unit/Test_InteractiveDataFriendRelation_Model.php
+++ tests/unit/Test_InteractiveDataFriendRelation_Model.php
@@ -9,6 +9,12 @@ class Test_InteractiveDataFriendRelation_Model extends \PHPUnit\Framework\TestCa
 		$this->assertInternalType('object', $test->GetFriendRelation("5a35e17dd7d69e7bfb31379e"));
 		$this->assertCount(1, $test->GetFriendRelation("5a35e518f3994baad05a85d2"));
 	}
+	public function test_GetFriendRelation_method2()
+	{
+		$test = new \InteractiveDataFriendRelation;
+		$this->assertNull("5a35e518f3994baad05a85");
+		$this->assertEmpty("5a35e518f3994baad05a85d");
+	}
 	
 }
 ?>
