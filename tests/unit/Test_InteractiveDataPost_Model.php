ca22d9a83c1e5ebe8bb6dc488a5261ceec3c4671
tungtran3012 <32742486+tungtran3012@users.noreply.github.com>
2017-12-25 05:06:18 +0700

    Update Test_InteractiveDataPost_Model.php

--- tests/unit/Test_InteractiveDataPost_Model.php
+++ tests/unit/Test_InteractiveDataPost_Model.php
@@ -15,4 +15,10 @@ class Test_InteractiveDataPost_Model extends \PHPUnit\Framework\TestCase
 	    	$this->assertInternalType('object', $test->getPost("5a35e518f3994baad05a85d2"));
 	    	$this->assertEmpty( $test->getPost("5a35e518f3994baad05a85d"));
 	    }
+	public function test_getPost_method3()
+	    {
+	    	$test = new \InteractiveDataPost;
+	    	$this->assertInternalType('object', $test->getPost("5a35e17dd7d69e7bfb31379e"));
+	    	$this->assertNotEmpty($test->getPost("5a35e518f3994baad05a85d2"));
+	    }
 }
